5f3f2a9dd748675d89d594bd7ddb813c665cc916
unknown <trey.hartley@Liono.envisagenow.com>
2015-03-06 13:42:49 -0500

    Add support to decode jwt in token response

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var GoogleAuth = require('./googleauth');
+var jwt = require('jwt-simple');
 
 process.env.PWD = process.cwd();
 var port = (process.env.PORT || 5000);
@@ -23,8 +24,15 @@ router.get('/env', function(req, res) {
 router.get('/auth/google', function(req, res, next) { googleAuth.authenticate(req, res, next); });
 router.get('/auth/google/callback', function(req, res, next) { googleAuth.extractToken(req, res, next); });
 
+router.get('/token/decode', function(req, res, next) {
+    console.log('> decode token: ' + req.query.token + '  secret: ' + process.env.GOOGLE_CLIENT_SECRET);
+    var token = jwt.decode(req.query.token, process.env.GOOGLE_CLIENT_SECRET, true);
+    res.send(token);
+});
+
 app.use('/api', router);
 app.use(express.static(process.env.PWD + '/public'));
+
 
 app.listen(port);
 console.log("Server running at localhost:" + port);
