62b28ce3a7607e52eaf8199691133145cd7aba5d
unknown <trey.hartley@Liono.envisagenow.com>
2015-03-06 12:59:29 -0500

    simplified google auth

---
+++ googleauth.js
@@ -0,0 +1,74 @@
+var qs = require('querystring');
+var unirest = require('unirest');
+
+function exchangeCodeForToken(code, opts, next) {
+    var params = {
+        'code': code,
+        'client_id': opts.clientId,
+        'client_secret': opts.clientSecret,
+        'redirect_url': opts.callbackUrl,
+        'grant_type': 'authorization_code'
+    };
+
+    unirest.post(opts.tokenUrl)
+        .header('Accept', 'application/json')
+        .type('application/x-www-form-urlencoded')
+        .send(qs.stringify(params))
+        .as.json(function(res) {
+            console.log('  - recieved auth token (' + res.status + '): ' + JSON.stringify(res.body));
+            if (res.statusType == 2) {
+                next.send(JSON.stringify(res.body));
+            }
+            else {
+                next.send(response.raw_body);
+            }
+        });
+}
+
+
+function GoogleAuth(opts) {
+    opts = opts || {};
+    this._options = {
+        authorizationUrl: opts.authorizationUrl || 'https://accounts.google.com/o/oauth2/auth',
+        tokenUrl: opts.tokenUrl || 'https://accounts.google.com/o/oauth2/token',
+        callbackUrl: opts.callbackUrl || 'http://trello-cal.herokuapp.com/api/auth/google/callback',
+        clientId: opts.clientId,
+        clientSecret: opts.clientSecret,
+        scope: opts.scope || []
+    };
+}
+
+module.exports = GoogleAuth;
+
+GoogleAuth.prototype.authenticate = function(req, res, next) {
+    var opts = this._options;
+
+    var params = {
+      'redirect_url': opts.callbackUrl,
+      'response_type': 'code',
+      'client_id': opts.clientId,
+      'scope': opts.scope.join(' '),
+      'access_type': 'offline',
+      'include_granted_scopes': true
+    };
+    var authUrl = opts.authorizationUrl + qs.stringify(params);
+    res.redirect(authUrl);
+    console.log('  - redirecting to: ' + authUrl);
+};
+
+
+GoogleAuth.prototype.extractToken = function(req, res, next) {
+    var opts = this._options;
+
+    if (req.query.error) {
+        console.log('  - recieved auth code error: ' + req.query.code);
+        next(req.query.error);
+    }
+    else if (req.query.code) {
+        console.log('  - recieved auth code: ' + req.query.code);
+        exchangeCodeForToken(req.query.code, opts, res);
+    }
+    else {
+        console.log('  - recieved unknown auth code: ' + req);
+    }
+};
--- index.js
+++ index.js
@@ -1,46 +1,18 @@
 var express = require('express');
 var app = express();
-var passport = require('passport');
-var googleStrategy = require('passport-oauth2').Strategy;
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var expressSession = require('express-session');
+var GoogleAuth = require('./googleauth');
 
 process.env.PWD = process.cwd();
 var port = (process.env.PORT || 5000);
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new googleStrategy({
-    authorizationURL: 'https://accounts.google.com/o/oauth2/auth',
-    tokenURL: 'https://accounts.google.com/o/oauth2/token',
-    callbackURL: 'http://trello-cal.herokuapp.com/api/auth/google/callback',
+var googleAuth = new GoogleAuth({
     clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  },
-  function(accessToken, refreshToken, profile, done) {
-    console.log('AccessToken: ' + accessToken);
-    console.log('RefreshToken: ' + refreshToken);
-    console.log('Profile: ' + JSON.stringify(profile));
-    return done(null, {
-      'accessToken': accessToken,
-      'refreshToken': refreshToken,
-      'profile': profile });
-  }
-));
-
-app.use(cookieParser);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(expressSession({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/calendar.readonly'
+    ]
+});
 
 var router = express.Router();
 
@@ -48,18 +20,8 @@ router.get('/env', function(req, res) {
   res.json({ env: process.env });
 });
 
-router.get('/auth/google',
-  passport.authenticate('oauth2', { scope: [
-    'https://www.googleapis.com/auth/calendar.readonly'
-  ] }),
-  function(req, res){ }
-);
-
-router.get('/auth/google/callback',
-  passport.authenticate('oauth2', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+router.get('/auth/google', function(req, res, next) { googleAuth.authenticate(req, res, next); });
+router.get('/auth/google/callback', function(req, res, next) { googleAuth.extractToken(req, res, next); });
 
 app.use('/api', router);
 app.use(express.static(process.env.PWD + '/public'));
