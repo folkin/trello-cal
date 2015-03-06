7018615093cee27b58e9d74754998245800242b9
Folkin <hartleyr@hotmail.com>
2015-03-06 01:13:03 -0500

    Test of google auth

--- server.js
+++ server.js
@@ -1,21 +1,56 @@
-
-console.log('env: ' + JSON.stringify(process.env));
-console.log('cwd: ' + process.cwd());
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+//var bodyParser = require('body-parser');
 
 process.env.PWD = process.cwd();
+var port = (process.env.PORT || 5000);
 
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var app = express();
-
-app.use(express.static(process.env.PWD + '/public'));
-
-http.createServer(app).listen((process.env.PORT || 5000));
-
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello from Trello-Cal\n");
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-console.log("Server running at localhost:" + app.get('port'));
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://trello-cal.herokuapp.com/api/auth/google/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log('AccessToken: ' + accessToken);
+    console.log('RefreshToken: ' + refreshToken);
+    console.log('Profile: ' + JSON.stringify(profile));
+    return done(null, {
+      'accessToken': accessToken,
+      'refreshToken': refreshToken,
+      'profile': profile });
+  }
+));
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+var router = express.Router();
+
+router.get('/env', function(req, res) {
+  res.json({ env: process.env });
+});
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/calendar.readonly'] }),
+  function(req, res){ }
+);
+
+router.get('auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.use('/api', router);
+app.use(express.static(process.env.PWD + '/public'));
+app.listen(port);
+console.log("Server running at localhost:" + port);
