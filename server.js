63bc3fbc8acf022a88a2971d592a3786cf35b918
Folkin <hartleyr@hotmail.com>
2015-03-05 23:24:44 -0500

    Testing static content

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-var port = (process.env.PORT || 5000);
+
+console.log('env: ' + JSON.stringify(process.env));
+console.log('cwd: ' + process.cwd());
+
+process.env.PWD = process.cwd();
+
+var express = require('express');
+var https = require('https');
 var http = require('http');
+var app = express();
+
+app.use(express.static(process.env.PWD + '/public'));
+
+http.createServer(app).listen((process.env.PORT || 5000));
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello from Trello-Cal\n");
 });
 
-server.listen(port);
-console.log("Server running at localhost:" + port);
-
+console.log("Server running at localhost:" + app.get('port'));
